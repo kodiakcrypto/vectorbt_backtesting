2eb3fe2dc43a3424efee193932cf99bbe31749e4
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-08 23:30:58 -0700

    Fixing Input Boxes
    Fixing Input Boxes

--- webapp.py
+++ webapp.py
@@ -32,29 +32,26 @@ def main():
             arguments = inspect.getfullargspec(ind_function) #get all params needed
             list_arg = {}
             for argument in arguments.args:
-                param_box_label = f"{argument}input" #name the input box
+                param_box = f"{argument}input" #name the input box
+                
                 # toggle type of input according to variable. Data will be automatically added, no need to enter infos
-
-                #check what datatype argument is
                 input_box_unique_id = ind_function.__name__+'_'+argument
-                arg_type = type(argument)
-                if arg_type == int:
-                    param_box_label = st.number_input(argument, step=1, key=input_box_unique_id)
-                elif arg_type == float:
-                    param_box_label = st.number_input(argument, step=0.1, key=input_box_unique_id)
-                elif arg_type == str:
-                    param_box_label = st.text_input(argument, key=input_box_unique_id)
-                elif arg_type == bool:
-                    param_box_label = st.checkbox(argument, key=input_box_unique_id)
-                else:
-                    param_box_label = st.text_input(argument, key=input_box_unique_id)
+                data_set = {"open_", "high", "low", "close", "volume"}
+                text_set = {"mamode"}
+                bool_set = {"talib"}
+                if argument in text_set:
+                    param_box = st.text_input(argument, key=input_box_unique_id)
+                elif argument in bool_set:
+                    param_box = st.radio(argument, options=(True, False), key=input_box_unique_id)
+                elif argument not in data_set:
+                    param_box = st.number_input(argument, step=1, key=input_box_unique_id)
 
-                list_arg[argument] = param_box_label
+                list_arg[argument] = param_box
             list_args[ind_function.__name__] = list_arg #multi indicator
         filename = ticker + "_" + str(amount_of_candles) + "_candles"
         def get_final_dataframe():
             calc_ind(filename, get_candles(ticker, amount_of_candles), col2, list_args)
-        st.button("Submit", on_click=get_final_dataframe)
+        st.button("'Go Go Go'", on_click=get_final_dataframe)
         
 
 
