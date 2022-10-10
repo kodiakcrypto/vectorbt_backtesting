5ae68061667b27504db1cdd2dc78055e0f8c5a31
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-10 02:32:46 -0700

    fix main columns
    fix main columns

--- webapp.py
+++ webapp.py
@@ -30,8 +30,6 @@ def main():
         if len(ind_functions) > 0: 
             st.write("\n# Indicator Params (leave unchanged to use defaults):")
 
-        b1, b2 = st.beta_columns([2,2])
-        is_b1 = True
         #list indicator parameter boxes
         for ind_function in ind_functions:
             st.sidebar.write(f"# {ind_function.__name__} Info")
@@ -39,36 +37,29 @@ def main():
             #add separator
             st.sidebar.write("\n# ===========\n")
 
-            #alternate columns
-            col = b2
-            if is_b1: col = b1
-            is_b1 = not is_b1
+            st.write(f"\n## {ind_function.__name__.title()}")
+            st.write(inspect.getfullargspec(ind_function))
+            args = inspect.getfullargspec(ind_function).args #get all params needed
+            args_dict = {}
+            for argument in args:
+                param_box = f"{argument}input" #name the input box
+                # toggle type of input according to variable. Data will be automatically added, no need to enter infos
+                input_box_unique_id = ind_function.__name__+'_'+argument
+                data_set = {"open_", "high", "low", "close", "volume"}
+                text_set = {"mamode"}
 
-            #get args
-            with col:
-                st.write(f"\n## {ind_function.__name__.title()}")
-                st.write(inspect.getfullargspec(ind_function))
-                args = inspect.getfullargspec(ind_function).args #get all params needed
-                args_dict = {}
-                for argument in args:
-                    param_box = f"{argument}input" #name the input box
-                    # toggle type of input according to variable. Data will be automatically added, no need to enter infos
-                    input_box_unique_id = ind_function.__name__+'_'+argument
-                    data_set = {"open_", "high", "low", "close", "volume"}
-                    text_set = {"mamode"}
+                if argument == "talib": param_box = False
+                elif argument == 'offset': param_box = 0
+                elif argument in text_set: param_box = st.text_input(argument, key=input_box_unique_id)
+                elif argument not in data_set: param_box = st.number_input(argument, step=1, key=input_box_unique_id)
 
-                    if argument == "talib": param_box = False
-                    elif argument == 'offset': param_box = 0
-                    elif argument in text_set: param_box = st.text_input(argument, key=input_box_unique_id)
-                    elif argument not in data_set: param_box = st.number_input(argument, step=1, key=input_box_unique_id)
-
-                    args_dict[argument] = param_box
-                args_dicts[ind_function.__name__] = args_dict #multi indicator
+                args_dict[argument] = param_box
+            args_dicts[ind_function.__name__] = args_dict #multi indicator
 
         filename = f"{ticker}_{timeframe}_{amount_of_candles}_candles"
         def get_final_dataframe():
             candles_dataframe = get_candles(ticker, timeframe, amount_of_candles)
-            calc_ind(filename, candles_dataframe, tiemframe, col2, args_dicts)
+            calc_ind(filename, candles_dataframe, timeframe, col2, args_dicts)
         st.button("Go Go Go", on_click=get_final_dataframe)
         
 
