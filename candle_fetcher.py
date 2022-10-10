8ba3c0079cb9cc69c28be2feb4f549a72285c3ed
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-10 02:37:21 -0700

    minor typo
    minor typo

--- candle_fetcher.py
+++ candle_fetcher.py
@@ -6,5 +6,5 @@ def get_candles(ticker="BTC-USD", timeframe="1d", amount_of_candles=1000):
         ticker, timeframe=timeframe, limit=amount_of_candles
     )
     cac_df.drop(labels=["Dividends", "Stock Splits"], axis=1, inplace=True)
-    cac_df.columns = [col.rstrip("_") for col in cac_df.columns]
+    cac_df.columns = [col.lower().rstrip("_") for col in cac_df.columns]
     return cac_df
