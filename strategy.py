685b8261dd40c6b75df9c2e04f424e7703f07604
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-10 02:13:02 -0700

    Adding UI backtesting functionality
    Adding UI backtesting functionality

--- strategy.py
+++ strategy.py
@@ -2,15 +2,18 @@ import pandas_ta as ta
 import streamlit as st
 import xlsxwriter
 from io import BytesIO
-import pandas as pd
 
-def calc_ind(filename, candle_dataframe, col, args_dicts):
+import finlab_crypto
+import pandas as pd
+import numpy as np
+
+def calc_ind(filename, candle_dataframe, timeframe, col, args_dicts):
     candle_dataframe.index = candle_dataframe.index.tz_localize(None)
 
     for ind_name, arg_dict in args_dicts.copy().items():
         for arg in arg_dict:
             if arg in ("open_", "high", "low", "close", "volume"):
-                args_dicts[ind_name][arg] = candle_dataframe[arg.title().rstrip("_")]
+                args_dicts[ind_name][arg] = candle_dataframe[arg.rstrip("_")]
         ind_function = getattr(ta, ind_name)
         res = ind_function(**arg_dict)
         candle_dataframe = pd.concat([candle_dataframe, res], axis=1)
@@ -18,14 +21,16 @@ def calc_ind(filename, candle_dataframe, col, args_dicts):
     # plot data
     with col:
         st.write(candle_dataframe)
+        _b1, _b2 = st.beta_columns([2,2])
         #download csv of data
-        st.download_button(
-            label="⬇️ CSV",
-            data=candle_dataframe.to_csv().encode("utf-8"),
-            file_name=f"{filename}.csv",
-            mime="text/csv",
-            help="Download CSV file",
-        )
+        with _b1:
+            st.download_button(
+                label="⬇️ CSV",
+                data=candle_dataframe.to_csv().encode("utf-8"),
+                file_name=f"{filename}.csv",
+                mime="text/csv",
+                help="Download CSV file",
+            )
 
         #download xlsx
         output = BytesIO()
@@ -34,11 +39,97 @@ def calc_ind(filename, candle_dataframe, col, args_dicts):
 
         worksheet.write('A1', filename)
         workbook.close()
-        
-        st.download_button(
-            label="⬇️ XLSX",
-            data=output.getvalue(),
-            file_name=f"{filename}.xlsx",
-            mime="application/vnd.ms-excel",
-            help="Download XLSX Excel file"
-        )
+        with _b2:
+            st.download_button(
+                label="⬇️ XLSX",
+                data=output.getvalue(),
+                file_name=f"{filename}.xlsx",
+                mime="application/vnd.ms-excel",
+                help="Download XLSX Excel file"
+            )
+
+
+    backtest_boxes = st.beta_expander('Backtest Options', expanded=False)
+    with backtest_boxes:
+        b1, b2 = st.beta_columns([2,2])
+        with b1:
+            long_short_both = st.selectbox('Long/Short/Both', ['long', 'short', 'both'])
+            amount_of_candles = st.number_input('# of Candles on chart', value=1000, min_value=1, max_value=10000, step=1)
+        with b2:
+            sl_start = st.number_input('SL Start', value=None, min_value=None, max_value=None, step=0.001)
+            sl_end = st.number_input('SL End', value=None, min_value=None, max_value=None, step=0.001)
+            sl_increment = st.number_input('SL Increment', value=None, min_value=None, max_value=None, step=0.001)
+        b3, b4 = st.beta_columns([2,2])
+        with b3:
+            tp_start = st.number_input('TP Start', value=None, min_value=None, max_value=None, step=0.001)
+            tp_end = st.number_input('TP End', value=None, min_value=None, max_value=None, step=0.001)
+            tp_increment = st.number_input('TP Increment', value=None, min_value=None, max_value=None, step=0.001)
+        with b4:
+            trail_start = st.number_input('Trail Start', value=None, min_value=None, max_value=None, step=0.001)
+            trail_end = st.number_input('Trail End', value=None, min_value=None, max_value=None, step=0.001)
+            trail_increment = st.number_input('Trail Increment', value=None, min_value=None, max_value=None, step=0.001)
+        if st.button('Run Backtest'):
+            backtest(
+                candle_dataframe, timeframe, long_short_both,
+                amount_of_candles=amount_of_candles,
+                sl_start=sl_start, sl_end=sl_end, sl_increment=sl_increment,
+                tp_start=tp_start, tp_end=tp_end, tp_increment=tp_increment,
+                trail_start=trail_start, trail_end=trail_end, trail_increment=trail_increment
+            )
+
+
+
+@finlab_crypto.Strategy()
+def strategy(candles_ta_dataframe, separate_panel_indicators): 
+    # give data to chart
+    figures = {
+      'figures': {
+        'overlaps': { #plot all decimal data columns other than ohlcv
+          col_name: candles_ta_dataframe[col_name] \
+            for col_name in candles_ta_dataframe.columns \
+              if col_name not in ['open', 'high', 'low', 'close', 'volume', 'entries', 'exits'] \
+                and type(candles_ta_dataframe[col_name].iloc[-1]) == np.float64
+        }
+      }
+    }
+
+    for col_name in separate_panel_indicators.columns:
+        figures[col_name] = { col_name: separate_panel_indicators[col_name] }
+
+    entries = candles_ta_dataframe['entries']
+    exits = candles_ta_dataframe['exits']
+
+    return entries, exits, figures
+
+def backtest(candles_dataframe, timeframe, long_short_both,
+             amount_of_candles=1000,
+             sl_start=None, sl_end=None, sl_increment=None,
+             tp_start=None, tp_end=None, tp_increment=None,
+             trail_start=None, trail_end=None, trail_increment=None,
+    ):
+
+    _vars = {}
+    # Get stoploss/takeprofit/trailing stop values
+    if sl_start and sl_end:
+      if not sl_increment: sl_increment = round(sl_end - sl_start, 3)
+      _vars['sl_stop'] = np.arange(sl_start, sl_end, sl_increment)
+    if tp_start and tp_end and tp_increment:
+      if not sl_increment: tp_increment = round(sl_end - sl_start, 3)
+      _vars['tp_stop'] = np.arange(tp_start, tp_end, tp_increment)
+    if trail_start and trail_end and trail_increment:
+      if not sl_increment: trail_increment = round(sl_end - sl_start, 3)
+      _vars['sl_trail'] = np.arange(trail_start, trail_end, trail_increment)
+
+    portfolio = strategy.backtest(
+        candles_dataframe,
+        _vars,
+        plot=True,
+        size=1,
+        init_cash='auto', 
+        freq=timeframe, 
+        side=long_short_both,
+        amount_of_candles=amount_of_candles,
+    )
+
+    st.write(portfolio.stats())
+    st.write(portfolio.plot())
--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ from candle_fetcher import get_candles
 def main():
     st.header("Data Downloader")
 
-    col1, col2 = st.columns([2, 5])
+    col1, col2 = st.columns([2, 3])
     # container: will be used by callback function calc_ind to populate the page
     cont = st.container()
     cont.write("")
@@ -30,6 +30,8 @@ def main():
         if len(ind_functions) > 0: 
             st.write("\n# Indicator Params (leave unchanged to use defaults):")
 
+        b1, b2 = st.beta_columns([2,2])
+        is_b1 = True
         #list indicator parameter boxes
         for ind_function in ind_functions:
             st.sidebar.write(f"# {ind_function.__name__} Info")
@@ -37,30 +39,36 @@ def main():
             #add separator
             st.sidebar.write("\n# ===========\n")
 
-            st.write(f"\n## {ind_function.__name__.title()}")
+            #alternate columns
+            col = b2
+            if is_b1: col = b1
+            is_b1 = not is_b1
 
-            args = inspect.getfullargspec(ind_function).args #get all params needed
-            args_dict = {}
-            for argument in args:
-                param_box = f"{argument}input" #name the input box
-                # toggle type of input according to variable. Data will be automatically added, no need to enter infos
-                input_box_unique_id = ind_function.__name__+'_'+argument
-                data_set = {"open_", "high", "low", "close", "volume"}
-                text_set = {"mamode"}
+            #get args
+            with col:
+                st.write(f"\n## {ind_function.__name__.title()}")
+                st.write(inspect.getfullargspec(ind_function))
+                args = inspect.getfullargspec(ind_function).args #get all params needed
+                args_dict = {}
+                for argument in args:
+                    param_box = f"{argument}input" #name the input box
+                    # toggle type of input according to variable. Data will be automatically added, no need to enter infos
+                    input_box_unique_id = ind_function.__name__+'_'+argument
+                    data_set = {"open_", "high", "low", "close", "volume"}
+                    text_set = {"mamode"}
 
-                if argument == "talib": param_box = False
-                elif argument == 'offset': param_box = 0
-                elif argument in text_set: param_box = st.text_input(argument, key=input_box_unique_id)
-                elif argument not in data_set: param_box = st.number_input(argument, step=1, key=input_box_unique_id)
+                    if argument == "talib": param_box = False
+                    elif argument == 'offset': param_box = 0
+                    elif argument in text_set: param_box = st.text_input(argument, key=input_box_unique_id)
+                    elif argument not in data_set: param_box = st.number_input(argument, step=1, key=input_box_unique_id)
 
-                args_dict[argument] = param_box
+                    args_dict[argument] = param_box
+                args_dicts[ind_function.__name__] = args_dict #multi indicator
 
-            args_dicts[ind_function.__name__] = args_dict #multi indicator
-
-        filename = f"{ticker}_{timeframe}_{amount_of_candles}_Candles"
+        filename = f"{ticker}_{timeframe}_{amount_of_candles}_candles"
         def get_final_dataframe():
             candles_dataframe = get_candles(ticker, timeframe, amount_of_candles)
-            calc_ind(filename, candles_dataframe, col2, args_dicts)
+            calc_ind(filename, candles_dataframe, tiemframe, col2, args_dicts)
         st.button("Go Go Go", on_click=get_final_dataframe)
         
 
