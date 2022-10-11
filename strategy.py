e49cd12c471e8de882b8ca1be9efe1793f139d75
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-11 03:34:24 -0700

    debug
    debug

--- strategy.py
+++ strategy.py
@@ -41,8 +41,8 @@ def strategy(candles_ta_dataframe):
 
     # entries = (candles_ta_dataframe['entries']   )
     # exits = (candles_ta_dataframe['exits']   ) 
-    entries = (candles_ta_dataframe.close > candles_ta_dataframe.close.shift(20) )
-    exits = (candles_ta_dataframe.close < candles_ta_dataframe.close.shift(20))
+    entries = None#(candles_ta_dataframe.close > candles_ta_dataframe.close.shift(20) )
+    exits = None#(candles_ta_dataframe.close < candles_ta_dataframe.close.shift(20))
     return entries, exits, {}#, figures
 
 def backtest(candles_dataframe, separate_panel_indicators, 
