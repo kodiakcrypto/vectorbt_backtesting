b1695106ec3c1438f22e00e874393aa9275df0f2
kodiakcrypto <62376262+kodiakcrypto@users.noreply.github.com>
2022-10-11 03:06:59 -0700

    debug
    debug

--- strategy.py
+++ strategy.py
@@ -25,7 +25,7 @@ def calc_ind(candle_dataframe, args_dicts):
 
 
 @finlab_crypto.Strategy(separate_panel_indicators=[])
-def strategy(ohlcv): 
+def strategy(candles_ta_dataframe): 
     # give data to chart
     # figures = { 
     #     'overlaps': { #plot all decimal data columns other than ohlcv
@@ -41,9 +41,9 @@ def strategy(ohlcv):
 
     # entries = (candles_ta_dataframe['entries']   )
     # exits = (candles_ta_dataframe['exits']   ) 
-    entries = (ohlcv.close > ohlcv.close.shift(20) )
-    exits = (ohlcv.close < ohlcv.close.shift(20))
-    return entries, exits#, figures
+    entries = (candles_ta_dataframe.close > candles_ta_dataframe.close.shift(20) )
+    exits = (candles_ta_dataframe.close < candles_ta_dataframe.close.shift(20))
+    return entries, exits, {}#, figures
 
 def backtest(candles_dataframe, separate_panel_indicators, 
              timeframe, long_short_both,
